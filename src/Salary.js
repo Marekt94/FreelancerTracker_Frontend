dd7f732eaa548c09baec8a412ad14729d8a689ab
Marekt94 <marekt94@gmail.com>
2023-08-13 23:42:10 +0200

    - usunięcie zbędnego importu

--- src/Salary.js
+++ src/Salary.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./Main.css";
 import "./SalaryAPICLient";
 import SalaryAPICLient from "./SalaryAPICLient";

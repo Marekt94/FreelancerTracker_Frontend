10bff299dd47d1a2ae70029adee61f24d454cfac
Marekt94 <marekt94@gmail.com>
2024-11-06 15:08:02 +0100

    login fixed, internalFetch created

--- src/Login.js
+++ src/Login.js
@@ -1,47 +1,40 @@
-import React, { useReducer, useState } from "react";
+import React, { useState } from "react";
 import { Edit } from "./MyComponents";
 import { hashSync } from "bcryptjs";
 import { useCookies } from "react-cookie";
 import { useAuthorize } from "./useAuthorize";
 
 export default function Login() {
-  const {authorize, logout} = useAuthorize();
+  const { authorize, logout } = useAuthorize();
   const [cookies] = useCookies();
-  const [authorized, setAuthorized] = useState(cookies.sessionId)
+  const [authorized, setAuthorized] = useState(cookies.sessionId);
 
   async function AuthorizateUser(login, haslo) {
     const hashedPassword = hashSync(haslo, "$2a$10$birC1iSgCy1pF17Oa7HXl.");
     setAuthorized(false);
-    await authorize(login, hashedPassword);
-    setAuthorized(true);
+    const res = await authorize(login, hashedPassword);
+    res && setAuthorized(true);
   }
-  
+
   async function Logout() {
     await logout();
     setAuthorized(false);
   }
-  
+
   function AuthorizeOrLogout(e, authorized) {
     e.preventDefault();
-  
+
     const formData = new FormData(e.target);
     const login = formData.get("login");
     const haslo = formData.get("haslo");
-  
+
     authorized ? Logout() : AuthorizateUser(login, haslo);
   }
 
   return (
     <form onSubmit={(e) => AuthorizeOrLogout(e, authorized)}>
-      <Edit
-        caption="Login"
-        name="login"
-      />
-      <Edit
-        caption="Hasło"
-        name="haslo"
-        password="true"
-      />
+      <Edit caption="Login" name="login" />
+      <Edit caption="Hasło" name="haslo" password="true" />
       <button type="submit">{authorized ? "Wyloguj" : "Zaloguj"}</button>
       {authorized && (
         <div>
--- src/useAuthorize.js
+++ src/useAuthorize.js
@@ -1,31 +1,32 @@
 import { SERVER_ADRESS } from "./Constants";
+import { useFetchInternal } from "./useFetchInternal";
 
-export function useAuthorize(){
-    async function authorize(login, password){
-        console.log(`login ${login} haslo ${password}`);
-        const URL = SERVER_ADRESS + "login";
-    
-        const requestOptions = {
-          method: "POST",
-          body: JSON.stringify({ username: login, password: password }),
-        };
-    
-        const res = await fetch(URL);
-        const data = await res.json();
-        return data;
-      }
-    
-      async function logout(){
-        const URL = SERVER_ADRESS + "logout";
-    
-        const requestOptions = {
-          method: "DELETE",
-        };
-    
-        const res = await fetch(URL);
-        const data = await res.json();
-        return data;
-      }
-    
-    return {authorize, logout};
+export function useAuthorize() {
+  const { internalFetch } = useFetchInternal();
+
+  async function authorize(login, password) {
+    console.log(`login ${login} haslo ${password}`);
+    const URL = SERVER_ADRESS + "login";
+
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify({ username: login, password: password }),
+    };
+
+    const data = await internalFetch(URL, requestOptions);
+    return data;
+  }
+
+  async function logout() {
+    const URL = SERVER_ADRESS + "logout";
+
+    const requestOptions = {
+      method: "DELETE",
+    };
+
+    const data = await internalFetch(URL, requestOptions);
+    return data;
+  }
+
+  return { authorize, logout };
 }
